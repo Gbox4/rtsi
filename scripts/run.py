3cc9a88fcea643d9cac106a4d9e48840c8852636
Ubuntu <ubuntu@ip-172-31-32-130.us-east-2.compute.internal>
2021-01-30 22:07:22 +0000

    changed run.py to analyze date range

--- scripts/run.py
+++ scripts/run.py
@@ -7,17 +7,17 @@ from analyze_comments import analyze_daily
 
 for day in range(1,24):
     target_date = datetime.date(2020, 12, day)
-    print(f"Fetching comments for {day}")
+    #print(f"Fetching comments for {day}")
 
-    try:
+    """try:
         comment_ids = get_comment_ids(get_daily_id(target_date=target_date))
     except Exception as e:
         if "Is the market open on this date?" in str(e):
             print("Thread not found, skipping...")
             continue
         else:
-            raise e
+            raise e"""
     
-    pull_comments(comment_ids)
+    #pull_comments(comment_ids)
 
-#analyze_daily(target_date)
+    analyze_daily(target_date)
