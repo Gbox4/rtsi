768fe2b0109b85f0537f79d864773251934d4f71
Gabe Banks <gabriel.t.banks@gmail.com>
2021-01-31 01:33:10 +0000

    used run.py to put ticker metadata into db

--- scripts/run.py
+++ scripts/run.py
@@ -1,23 +1,43 @@
 import datetime
 from scrape_daily import get_daily_id, get_comment_ids, pull_comments
 from analyze_comments import analyze_daily
+import pathlib
+import sqlite3
+current_directory = str(pathlib.Path(__file__).parent.absolute())
 
 
 #target_date = datetime.date(int(input("Year: ")), int(input("Month: ")), int(input("Day: ")))
 
-for day in range(1,5):
+"""for day in range(1,5):
     target_date = datetime.date(2020, 12, day)
-    #print(f"Fetching comments for {day}")
+    analyze_daily(target_date)"""
 
-    """try:
-        comment_ids = get_comment_ids(get_daily_id(target_date=target_date))
-    except Exception as e:
-        if "Is the market open on this date?" in str(e):
-            print("Thread not found, skipping...")
-            continue
-        else:
-            raise e"""
-    
-    #pull_comments(comment_ids)
+common_words = []
+f = open(f"{current_directory}/../data/common_words.txt", "r")
+for line in f:
+    common_words.append(line.replace("\n", ""))
+f.close()
 
-    analyze_daily(target_date)
+conn = sqlite3.connect(f"{current_directory}/../tickerdat.db")
+c = conn.cursor()
+
+with open(f"{current_directory}/../data/nasdaq_tickers.csv", "r") as f:
+    for line in f:
+        ticker = line.split(",")[0]
+        common_word = ticker in common_words
+        exchange = "NASDAQ"
+        obscure = False
+        c.execute(f"INSERT INTO ticker_metadata VALUES (?,?,?,?)", (ticker,common_word,exchange,obscure))
+
+
+with open(f"{current_directory}/../data/nyse_tickers.csv", "r") as f:
+    for line in f:
+        ticker = line.split(",")[0]
+        common_word = ticker in common_words
+        exchange = "NYSE"
+        obscure = False
+        c.execute(f"INSERT INTO ticker_metadata VALUES (?,?,?,?)", (ticker,common_word,exchange,obscure))
+
+
+conn.commit()
+conn.close()
